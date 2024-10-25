7ec1a8584fbed3be2fc96acad5ff721d0e6b9a45
vuducCentrala <duc.vu@centrala.vn>
2024-10-25 16:07:52 +0700

    JSX

--- chuong-01/src/components/Lesson.js
+++ chuong-01/src/components/Lesson.js
@@ -1,17 +1,36 @@
 // import React from "react";
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
 // Method 1
+function formatName(user){
+    return user.firstName + ' ' + user.lastName
+}
+
 function Lesson() {
+    const user = {
+        firstName : 'Vu',
+        lastName : 'Duc 11'
+    }
+    
   return (
-    <li className="list-group-item">Text</li>
+    <li className="list-group-item">{formatName(user)}</li>
   );
 }
 
 // Method 2
 // class Lesson extends Component {
+
+//     formatName(user){
+//         return user.firstName + ' ' + user.lastName
+//     }
+
 //     render() {
-//       return React.createElement('li', { className: 'list-group-item' }, 'Text 2');
+//         const user = {
+//             firstName : 'Vu',
+//             lastName : 'Duc 2'
+//         }
+
+//       return React.createElement('li', { className: 'list-group-item' }, this.formatName(user));
 //     }
 // }
   
