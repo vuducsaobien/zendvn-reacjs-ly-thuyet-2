ae17874c6fd0ef9fe73341cc5e69554953d8b948
vuducCentrala <duc.vu@centrala.vn>
2024-10-25 15:13:51 +0700

    Course Component

--- chuong-01/src/App.js
+++ chuong-01/src/App.js
@@ -1,81 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Course from './components/Course';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload. 1234
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-
-    //   <header className="App-header">
-    //     <h2>Welcome to ReactJs</h2>
-    //   </header>
-
-
-    // </div>
-
-
     <div className="row">
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <div className="panel panel-info">
-              <div className="panel-heading">
-                <h3 className="panel-title">React JS</h3>
-              </div>
-              <div className="panel-body">
-                <ul className="list-group">
-                  <li className="list-group-item">Text</li>
-                  <li className="list-group-item">Text</li>
-                  <li className="list-group-item">Text</li>
-                </ul>
-              </div>
-          </div>
-      </div>
-
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <div className="panel panel-info">
-              <div className="panel-heading">
-                <h3 className="panel-title">React JS</h3>
-              </div>
-              <div className="panel-body">
-                <ul className="list-group">
-                  <li className="list-group-item">Text</li>
-                  <li className="list-group-item">Text</li>
-                  <li className="list-group-item">Text</li>
-                </ul>
-              </div>
-          </div>
-      </div>
-
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <div className="panel panel-info">
-              <div className="panel-heading">
-                <h3 className="panel-title">React JS</h3>
-              </div>
-              <div className="panel-body">
-                <ul className="list-group">
-                  <li className="list-group-item">Text</li>
-                  <li className="list-group-item">Text</li>
-                  <li className="list-group-item">Text</li>
-                </ul>
-              </div>
-          </div>
-      </div>
+        <Course />
+        <Course />
+        <Course />
+        <Course></Course>
   </div>
 
-
-    
   );
 }
 
---
+++ chuong-01/src/components/Course.js
@@ -0,0 +1,22 @@
+
+
+function Course() {
+  return (
+    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div className="panel panel-info">
+            <div className="panel-heading">
+                <h3 className="panel-title">React JS</h3>
+            </div>
+            <div className="panel-body">
+                <ul className="list-group">
+                <li className="list-group-item">Text</li>
+                <li className="list-group-item">Text</li>
+                <li className="list-group-item">Text</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Course;
