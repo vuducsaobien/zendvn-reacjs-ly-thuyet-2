34b65aeaefbce40b5edda32a5b0d27cbb6701009
vuducCentrala <duc.vu@centrala.vn>
2024-10-25 15:03:32 +0700

    AppComponent

--- chuong-01/src/App.js
+++ chuong-01/src/App.js
@@ -3,22 +3,79 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 1234
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload. 1234
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+
+    //   <header className="App-header">
+    //     <h2>Welcome to ReactJs</h2>
+    //   </header>
+
+
+    // </div>
+
+
+    <div className="row">
+      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div className="panel panel-info">
+              <div className="panel-heading">
+                <h3 className="panel-title">React JS</h3>
+              </div>
+              <div className="panel-body">
+                <ul className="list-group">
+                  <li className="list-group-item">Text</li>
+                  <li className="list-group-item">Text</li>
+                  <li className="list-group-item">Text</li>
+                </ul>
+              </div>
+          </div>
+      </div>
+
+      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div className="panel panel-info">
+              <div className="panel-heading">
+                <h3 className="panel-title">React JS</h3>
+              </div>
+              <div className="panel-body">
+                <ul className="list-group">
+                  <li className="list-group-item">Text</li>
+                  <li className="list-group-item">Text</li>
+                  <li className="list-group-item">Text</li>
+                </ul>
+              </div>
+          </div>
+      </div>
+
+      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+          <div className="panel panel-info">
+              <div className="panel-heading">
+                <h3 className="panel-title">React JS</h3>
+              </div>
+              <div className="panel-body">
+                <ul className="list-group">
+                  <li className="list-group-item">Text</li>
+                  <li className="list-group-item">Text</li>
+                  <li className="list-group-item">Text</li>
+                </ul>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+    
   );
 }
 
--- chuong-01/src/index.js
+++ chuong-01/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import App from './App';
+// import Hello from './components/Hello';
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // id=root
 root.render(
   <React.StrictMode>
-    <Hello /> {/* App component at src/App.js */}
+    <App /> {/* App component at src/App.js */}
   </React.StrictMode>
 );
 
