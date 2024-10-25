2e46e2a9b3942aa1d0dc6aedd1d80afc0401a0d8
vuducCentrala <duc.vu@centrala.vn>
2024-10-25 16:50:27 +0700

    props

--- chuong-01/src/App.js
+++ chuong-01/src/App.js
@@ -5,11 +5,10 @@ import Course from './components/Course';
 function App() {
   return (
     <div className="row">
-        <Course />
-        <Course />
-        <Course />
-        <Course></Course>
-  </div>
+        <Course name='React JS' time='30h' free={true} >React JS is very simple</Course>
+        <Course name='Angular 4x' time='55h' free={false}/>
+        <Course name='Node JS' time='35h' free={false}/>
+    </div>
 
   );
 }
--- chuong-01/src/components/Course.js
+++ chuong-01/src/components/Course.js
@@ -1,19 +1,48 @@
 import Lesson from './Lesson';
 
-function Course() {
+/* Method 3 */
+function showButtonFree(free) {
+    if (free) {
+        return <button className="btn btn-warning" type="button">View</button>;
+    }
+}
+
+function Course(props) {
+
+    /* Method 2 */
+    // const showButtonFree = () => {
+    //     return props.free ? <button className="btn btn-warning" type="button">View</button> : null;
+    // };
+
   return (
     <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <div className="panel panel-info">
             <div className="panel-heading">
-                <h3 className="panel-title">React JS</h3>
+                <h3 className="panel-title">{props.name}</h3>
             </div>
             <div className="panel-body">
+                <p>{props.time}</p>
+                <p>{props.children}</p>
+
                 <ul className="list-group">
                     <Lesson />
                 </ul>
             </div>
+
+            {/* Method 1 */}
+            {/* {props.free && (
+                <div className='panel-footer'>
+                    <button  className="btn btn-warning" type="button">View</button>
+                </div>
+            )} */}
+
+            {/* Method 2 */}
+            {/* {showButtonFree()} */}
+
+            {/* Method 3 */}
+            {showButtonFree(props.free)}
+
         </div>
-        {/* <button  className="btn btn-success" type="button">Register</button> */}
     </div>
   );
 }
