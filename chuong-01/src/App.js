dbc09746f87319ee06c4123c87b21d1a46906a8d
vuducCentrala <duc.vu@centrala.vn>
2024-10-28 10:23:40 +0700

    JSX foreach

--- chuong-01/src/App.js
+++ chuong-01/src/App.js
@@ -3,11 +3,42 @@ import './App.css';
 import Course from './components/Course';
 
 function App() {
+  const items = [
+        {
+            name: 'ReacjJS',
+            time: '30h',
+            free: true,
+            desc: 'ReacjJS is very simple'
+        },
+        {
+            name: 'Angular 4x',
+            time: '55h',
+            free: false
+        },
+        {
+            name: 'Node JS',
+            time: '35h',
+            free: true
+        }
+    ];
+
+    const eleItem = items.map((item, index) => {
+        // console.log(item);
+        // console.log('items = ');
+
+        return <Course key={index} name={item.name} time={item.time} free={item.free}>{item.desc}</Course>
+    });
+
+    // console.log(items);
+    // console.log('items = ');
+
   return (
     <div className="row">
-        <Course name='React JS' time='30h' free={true} >React JS is very simple</Course>
+        {/* <Course name='React JS' time='30h' free={true} >React JS is very simple</Course>
         <Course name='Angular 4x' time='55h' free={false}/>
-        <Course name='Node JS' time='35h' free={false}/>
+        <Course name='Node JS' time='35h' free={false}/> */}
+
+        {eleItem}
     </div>
 
   );
