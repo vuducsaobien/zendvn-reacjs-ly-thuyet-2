102737455e387b6c806de6e6bff10a2ef99a70d1
vuducCentrala <duc.vu@centrala.vn>
2024-10-30 15:24:39 +0700

    add uuid

--- chuong-02/src/mock/tasks.js
+++ chuong-02/src/mock/tasks.js
@@ -1,14 +1,24 @@
+import { v4 as uuidv4 } from 'uuid';
+
 let mockData = [
     {
+        id: uuidv4(),
         name: 'A',
         level: 0
     },
     {
+        id: uuidv4(),
         name: 'B',
         level: 1
     },
     {
+        id: uuidv4(),
         name: 'C',
+        level: 2
+    },
+    {
+        id: uuidv4(),
+        name: 'D',
         level: 2
     }
 ];
