4a1db9f2c48e25c6dbbbedd94cfba4c71f0c517b
vuducCentrala <duc.vu@centrala.vn>
2024-11-14 14:53:02 +0700

    clear input search button

--- chuong-02/src/components/Search.js
+++ chuong-02/src/components/Search.js
@@ -1,5 +1,5 @@
 // import { getLevel } from "../helpers/level";
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 
 function Search() {
@@ -8,25 +8,40 @@ function Search() {
     // Create a ref for the username input
     const searchRef = useRef(null);
 
+    // States
+    const [strSearchSearch, setStrSearchSearch] = useState('123');
+    
+
     function handleSearch(search){
         console.log('handleSearch');
         console.log(search);
+    }
+
+    function handleClear(){
+        setStrSearchSearch('');
     }
 
     return (
         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
             <div className="input-group">
                 <input
-                type="text"
-                className="form-control"
-                // ref="search"
-                ref={searchRef}
-                placeholder="Search for..."
+                    type="text"
+                    className="form-control"
+                    // ref="search"
+                    ref={searchRef}
+                    placeholder="Search for..."
+                    value={strSearchSearch}
+                    onChange={(e) => setStrSearchSearch(e.target.value)}
                 />
                 <span className="input-group-btn">
                 <button onClick={() => handleSearch(searchRef.current.value)} className="btn btn-info" type="button">
                     Go!
                 </button>
+
+                <button onClick={() => handleClear()} className="btn btn-danger" type="button">
+                    Clear
+                </button>
+
                 </span>
             </div>
         </div>
