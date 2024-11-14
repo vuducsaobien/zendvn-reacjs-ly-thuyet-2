c7fb42a1e255f8b3b2c2e7d51ba09d1d831e5627
vuducCentrala <duc.vu@centrala.vn>
2024-11-14 14:33:34 +0700

    search ref input

--- chuong-02/src/components/Search.js
+++ chuong-02/src/components/Search.js
@@ -1,8 +1,17 @@
 // import { getLevel } from "../helpers/level";
+import { useRef } from 'react';
 
 
 function Search() {
     // const { index, name, level } = props;
+
+    // Create a ref for the username input
+    const searchRef = useRef(null);
+
+    function handleSearch(search){
+        console.log('handleSearch');
+        console.log(search);
+    }
 
     return (
         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -11,10 +20,11 @@ function Search() {
                 type="text"
                 className="form-control"
                 // ref="search"
+                ref={searchRef}
                 placeholder="Search for..."
                 />
                 <span className="input-group-btn">
-                <button className="btn btn-info" type="button">
+                <button onClick={() => handleSearch(searchRef.current.value)} className="btn btn-info" type="button">
                     Go!
                 </button>
                 </span>
