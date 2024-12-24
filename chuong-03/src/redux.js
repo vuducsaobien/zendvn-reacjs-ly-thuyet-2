16483380f4e159d8da0dcba91776969a3d20e60e
vuducCentrala <duc.vu@centrala.vn>
2024-12-24 11:13:26 +0700

    reducer change state isShowForm

--- chuong-03/src/redux.js
+++ chuong-03/src/redux.js
@@ -3,42 +3,27 @@ import { createSlice, configureStore } from '@reduxjs/toolkit';
 // Set States App.js
 let initialState = {
     items : [],
-    isShowForm : false,
+    isShowForm : true,
     strSearchApp : '',
     orderBy : 'name',
     orderDir : 'asc',
     itemSelected : null,
 };
 
-/* 
-let appReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'CLOSE_FORM':
-            console.log('CLOSE_FORM', state);
-            break;
-    
-        default:
-            return state;
-    }
-    // return state;
-};
-*/
-
-/* 
-    let appReducer = {
-        
-    };  
-*/
-
-
 const appSlice = createSlice({
     name: 'app',
     initialState,
-
     reducers : {
         CLOSE_FORM: (state) => {
-            console.log('CLOSE_FORM');
-            console.log(state);
+            state.isShowForm = false;
+            return state;
+        },
+        OPEN_FORM: (state) => {
+            state.isShowForm = true;
+            return state;
+        },
+        TOOGLE_FORM: (state) => {
+            state.isShowForm = !state.isShowForm;
             return state;
         }
     }
@@ -48,17 +33,19 @@ const store = configureStore({
     reducer: appSlice.reducer
 });
 
-console.log(store);
-// console.log(store.getState());
-
 // Export các actions để sử dụng
-export const { CLOSE_FORM } = appSlice.actions;
+export const { CLOSE_FORM, OPEN_FORM, TOOGLE_FORM } = appSlice.actions;
+
+console.log('init', store.getState().isShowForm); // Init ; isShowForm = true
 
 store.dispatch(CLOSE_FORM());
+console.log('CLOSE FORM', store.getState().isShowForm); // isShowForm phai = false
+
+store.dispatch(TOOGLE_FORM());
+console.log('TOOGLE FORM', store.getState().isShowForm); // isShowForm phai = true
+
+store.dispatch(OPEN_FORM());
+console.log('OPEN FORM', store.getState().isShowForm); // isShowForm phai = true
 
 
 export default store;
-
-// khi các initialState thay đổi  
-// => gọi action truyền vào state 
-// => thông qua appReducer trả về state mới
